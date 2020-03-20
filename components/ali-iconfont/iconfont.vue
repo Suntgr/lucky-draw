331dfc3b84441cb56e9021cf0e971c72500b898e
suntgr <suntgr@163.com>
2020-03-20 18:19:34 +0800

    字体图标组件修改

--- components/ali-iconfont/iconfont.vue
+++ components/ali-iconfont/iconfont.vue
@@ -2,7 +2,6 @@
   <text 
     class="iconfont" 
     :class="'icon-' + icon" 
-    :style="{ color: color, fontSize: size + 'px' }" 
     @click="onClick">
   </text>
 </template>
@@ -11,9 +10,7 @@
   /**
    * iconfont 图标
    * @description 用于展示 iconfont 图标
-   * @property {Number} size 图标大小
-   * @property {String} type 图标图案，参考示例
-   * @property {String} color 图标颜色
+   * @property {String} icon 图标图案，参考示例
    * @event {Function} click 点击 Icon 触发事件
    */
   export default {
@@ -22,14 +19,6 @@
       icon: {
         type: String,
         default: ''
-      },
-      size: {
-        type: Number,
-        default: 16
-      },
-      color: {
-        type: String,
-        default: '#333333'
       }
     },
     data() {
@@ -43,6 +32,14 @@
   };
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
   @import url('./iconfont.css');
+  .iconfont {
+    font-family: "iconfont" !important;
+    font-style: normal;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    color: $mainColor;
+    font-size: 32rpx;
+  }
 </style>
--- pages/product/draw.vue
+++ pages/product/draw.vue
@@ -1,12 +1,93 @@
 <template>
-  <view>
-    发起抽奖
-    <icon-font icon="right" color="blue" size="20"></icon-font>
+  <view class="draw">
+    <view class="group">
+      <image class="banner" src="" mode="aspectFill"></image>
+      <view class="cell">
+        <icon-font icon="gift"></icon-font>
+        <text class="cell-name">抽奖类型</text>
+        <view class="switch">
+          <view class="btn">普通</view>
+          <view class="btn">趣味</view>
+        </view>
+      </view>
+      <view class="cell">
+        <icon-font icon="gift"></icon-font>
+        <text class="cell-name">奖品名称</text>
+        <input class="cell-input" type="text" placeholder="请输入奖品名称">
+      </view>
+      <view class="cell">
+        <icon-font icon="gift"></icon-font>
+        <text class="cell-name">奖品份数</text>
+        <input class="cell-input" type="number" placeholder="请输入份数">
+      </view>
+    </view>
   </view>
 </template>
 
 <script>
 </script>
 
-<style>
+<style lang="scss" scoped>
+  .draw /deep/ {
+    width: 100%;
+    box-sizing: border-box;
+    min-height: 100vh;
+    background-color: #f0f0f0;
+    padding: 40rpx 20rpx 0;
+
+    .group {
+      width: auto;
+      background-color: #FFFFFF;
+      box-shadow: 0px 0px 8rpx 0px rgba(0, 0, 0, 0.1);
+      border-radius: 8rpx;
+      padding: 30rpx;
+
+      .banner {
+        width: 100%;
+        height: 340rpx;
+        background: #C56C62;
+      }
+
+      .cell {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        margin-top: 20rpx;
+        height: 112rpx;
+        position: relative;
+        &::before {
+          content: '';
+          position: absolute;
+          bottom: 0;
+          left: 42rpx;
+          right: -30rpx;
+          height: 1px;
+          background:rgba(0,0,0,0.1);
+        }
+      }
+
+      .cell-name {
+        margin-right: auto;
+        font-size: 14px;
+        margin-left: 10rpx;
+      }
+
+      .switch {
+        .btn {
+          display: inline-block;
+          width: 132rpx;
+          height: 60rpx;
+          text-align: center;
+          line-height: 60rpx;
+          color: #E57170;
+          border: 1rpx solid;
+          border-radius: 8rpx;
+          background: rgba(217, 52, 51, 0.1)
+        }
+      }
+      .cell-input {
+        text-align: right;
+      }
+    }
+  }
 </style>
